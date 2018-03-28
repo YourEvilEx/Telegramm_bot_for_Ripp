e82dfdd2ee864d6764d8afa4e992090f56342d63
YourEvilEx <peshkow2015@mail.ru>
2018-03-28 23:33:27 +0700

    Исправил ошибку с командами бота

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def log(message, answer):
     print('Ответ = ', answer)
 
 
-@bot.message_handler(command=['start'])
+@bot.message_handler(commands=['start'])
 def repeat_start(message_st):
         answer = 'Привет, я простой бот который умеет только одно, авторизировать пользователей. ' \
                  'Пожалуйста пройди авторизацию.'
@@ -37,14 +37,14 @@ def repeat_start(message_st):
         log(message_st, answer)
 
 
-@bot.message_handler(command=['help'])
+@bot.message_handler(commands=['help'])
 def repeat_help(message_hl):
     answer = 'Отправь команду /auth для авторизации и мы продолжим работу.'
     bot.send_message(message_hl.chat.id, answer)
     log(message_hl, answer)
 
 
-@bot.message_handler(command=['auth'])
+@bot.message_handler(commands=['auth'])
 def repeat_auth(message_au):
     if const.first_name == message_au.from_user.first_name and const.last_name == message_au.from_user.last_name:
         bot.send_message(message_au.chat.id, const.ans_flag)
@@ -54,13 +54,12 @@ def repeat_auth(message_au):
         log(message_au, const.ans_no_flag)
 
 
-'''
 @bot.message_handler(content_types=['text'])
 def repeat_all_message(message):
     answer = 'Я бы с удовольствием с тобой по общался, но не запрограммирован на это.\n' \
              'Пожалуйста пройди авторизацию, не делай мне больно.'
     bot.send_message(message.chat.id, answer)
     log(message, answer)
-'''
+
 
 bot.polling(none_stop=True, interval=0)
