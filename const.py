2fa9cfe4ddf60c8a4694eaaf1b22b39f6cfef21c
YourEvilEx <peshkow2015@mail.ru>
2018-04-06 22:35:33 +0700

    Исправил флаг

--- const.py
+++ const.py
@@ -7,5 +7,5 @@ date_of_birth = '01.01.1983'
 date_of_registration = '06.04.2018'
 
 
-ans_flag = 'escctf{I_congratulate_you_the_son_of_my_mother\'s_friend}'
+ans_flag = 'escctf{I_congratulate_you_the_son_of_my_mother_s_friend}'
 ans_no_flag = 'Прости, но увы у тебя нет доступа, возвращайся как получишь его.'
